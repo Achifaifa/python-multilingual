69fd720a963226e7bf5d87ea123ca6e5f7acbccf
Yuri Numerov <achi@hush.ai>
2016-07-03 18:36:06 +0200

    added template and spanish

--- empty.py
+++ empty.py
@@ -3,4 +3,33 @@ empty={/
 "PRINTEXP": "", #print
 "DELEXP": "", #del
 "PASSEXP": "", #pass
+"BREAKEXP": "", #break
+"CONTINUEEXP": "", #continue
+"RETURNEXP": "", #return
+"RAISEEXP": "", #raise
+"IMPORTEXP": "", #import
+"IMPORTFROMEXP": "", #from
+"IMPORTASEXP": "", #as
+"GLOBALEXP": "", #global
+"EXECEXP": "", #exec
+"INEXP": "", #in
+"ASSERTEXP": "", #assert
+"IFEXP": "", #if
+"ELIFEXP": "", #elif
+"ELSEEXP": "", #else
+"WHILEEXP": "", #while
+"FOREXP": "", #for
+"TRYEXP": "", #try
+"FINALLYEXP": "", #finally
+"WITHEXP": "", #with
+"WITHASEXP": "", #as
+"EXCEPTEXP": "", #except
+"EXCEPTASEXP": "", #as
+"LAMBDAEXP": "", #lambda
+"OREXP": "", #or
+"ANDEXP": "", #and
+"NOTEXP": "", #not
+"ISEXP": "", #is
+"CLASSEXP": "", #class
+
 }
---
+++ spanish.py
@@ -0,0 +1,35 @@
+spanish={/
+# marker : translated expression # original python keyword
+"PRINTEXP": "imprimir", #print
+"DELEXP": "borrar", #del
+"PASSEXP": "pasar", #pass
+"BREAKEXP": "salir", #break
+"CONTINUEEXP": "continuar", #continue
+"RETURNEXP": "devolver", #return
+"RAISEEXP": "avisar", #raise
+"IMPORTEXP": "importar", #import
+"IMPORTFROMEXP": "de", #from
+"IMPORTASEXP": "como", #as
+"GLOBALEXP": "global", #global
+"EXECEXP": "ejecutar", #exec
+"INEXP": "en", #in
+"ASSERTEXP": "comprobar", #assert
+"IFEXP": "si", #if
+"ELIFEXP": "ysi", #elif
+"ELSEEXP": "ysino", #else
+"WHILEEXP": "mientras", #while
+"FOREXP": "para cada", #for
+"TRYEXP": "intentar", #try
+"FINALLYEXP": "finalmente", #finally
+"WITHEXP": "con", #with
+"WITHASEXP": "como", #as
+"EXCEPTEXP": "excepto", #except
+"EXCEPTASEXP": "como", #as
+"LAMBDAEXP": "lambda", #lambda
+"OREXP": "o", #or
+"ANDEXP": "y", #and
+"NOTEXP": "no", #not
+"ISEXP": "es", #is
+"CLASSEXP": "clase", #class
+
+}
