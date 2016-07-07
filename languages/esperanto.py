63aab5940adefdd4046071915aaf685aa3eb52ac
Yuri Numerov <achi@hush.ai>
2016-07-07 22:14:57 +0200

    added esperanto (beta)

---
+++ languages/esperanto.py
@@ -0,0 +1,34 @@
+# marker : translated expression # original python keyword
+esperanto={"PRINTEXP": "presi", #print
+"DEFEXP": "difini", #define
+"DELEXP": "forvisi", #del
+"PASSEXP": "pasigi", #pass
+"BREAKEXP": "rompi", #break
+"CONTINUEEXP": "daurigi", #continue
+"RETURNEXP": "reiri", #return
+"RAISEEXP": "levigi", #raise
+"IMPORTEXP": "alporti", #import
+"IMPORTFROMEXP": "el", #from
+"IMPORTASEXP": "kiel", #as
+"GLOBALEXP": "malloka", #global
+"EXECEXP": "ruli", #exec
+"INEXP": "en", #in
+"ASSERTEXP": "aserti", #assert
+"IFEXP": "se", #if
+"ELIFEXP": "elif", #elif #TODO
+"ELSEEXP": "alia", #else
+"WHILEEXP": "dum", #while
+"FOREXP": "por", #for
+"TRYEXP": "provi", #try
+"FINALLYEXP": "fine", #finally
+"WITHEXP": "kun", #with
+"WITHASEXP": "kiel", #as
+"EXCEPTEXP": "krom", #except
+"EXCEPTASEXP": "kiel", #as
+"LAMBDAEXP": "lambda", #lambda
+"OREXP": "au", #or
+"ANDEXP": "kaj", #and
+"NOTEXP": "ne", #not
+"ISEXP": "estas", #is
+"CLASSEXP": "tipo", #class
+}
